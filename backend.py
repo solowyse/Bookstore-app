4a5afbfc25ed8acef265943e3f8cbd657ce9549b
Solomon Njoku <32582473+solowyse@users.noreply.github.com>
2018-08-22 13:24:02 +0100

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,)) # this comma here must be present, or you have an unreasonable error.
+    conn.commit()
+    conn.close()
+
+def update(id, title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+
+#connect()
+#insert("Incorruptible Judge", "Amaka Ibe", 1998, 891008923)
+#insert("The Concubine", "Chinua Achebe", 2000, 78912690)
+#delete(2)
+#update(1, "The Sea of Hope", "John Smooth", 1918, 913123189)
+#print(view())
